ec10e3071bb9ac21412f398ab9f994d22121c9ed
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-05-30 10:55:44 -0300

    Adding info plist bullshit

--- MiniChallenge8/ViewControllers/BattleGameViewController.swift
+++ MiniChallenge8/ViewControllers/BattleGameViewController.swift
@@ -9,27 +9,22 @@
 import UIKit
 
 class BattleGameViewController: UIViewController {
+    
+    var currentState: State!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.currentState = StartBattleState(viewController: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func moveTo(state: State) {
+        if let didExitState = self.currentState.didExitState {
+            didExitState()
+        }
+        self.currentState = state
+        if let didEnterState = self.currentState.didEnterState {
+            didEnterState()
+        }
     }
-    */
 
 }
